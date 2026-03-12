ba8fed3f6cb0fa5d225c1f1c00388367f79c5f1a
Kaua Barroso <115954731+Ka1ux@users.noreply.github.com>
2026-03-12 05:53:09 -0300

    Add files via upload

---
+++ 1/__pycache__/teste_b.py
@@ -0,0 +1,12 @@
+import json
+
+from teste_a import CAMINHO_ARQUIVO, Pessoa, fazer_dump
+
+
+with open(CAMINHO_ARQUIVO, 'r') as arquivo:
+    pessoas = json.load(arquivo)
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+
+    print(p1.nome, p1.idade)
+    print(p2.nome, p2.idade)
